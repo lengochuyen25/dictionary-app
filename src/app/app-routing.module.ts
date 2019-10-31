f3622ab6f3136a2b1f882daf60c2a14e2de31274
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-10-31 17:21:09 +0700

    you can do it!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,47 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule, PreloadAllModules} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {DictionaryPageComponent} from "./dictionary-page/dictionary-page.component";
+import {DictionaryDetailComponent} from "./dictionary-detail/dictionary-detail.component";
+import {YoutubePlaylistComponent} from "./youtube-playlist/youtube-playlist.component";
+import {YoutubePlayerComponent} from "./youtube-player/youtube-player.component";
+import {DictionaryComponent} from "./dictionary/dictionary.component";
+import {TimelinesComponent} from "./timelines/timelines.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+  {
+    path: 'dictionary',
+    component: DictionaryComponent,
+    children: [
+      {
+        path: ':key',
+        component: DictionaryDetailComponent,
+      }
+    ]
+  },
+  {
+    path: 'youtube',
+    component: YoutubePlaylistComponent,
+    children: [{
+      path: ':id',
+      component: YoutubePlayerComponent
+    }]
+  },
+  {
+    path: 'timelines',
+    component: TimelinesComponent
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { YoutubePlaylistComponent } from './youtube-playlist/youtube-playlist.component';
+import { YoutubePlayerComponent } from './youtube-player/youtube-player.component';
+import { DictionaryComponent } from './dictionary/dictionary.component';
+import { DictionaryPageComponent } from './dictionary-page/dictionary-page.component';
+import { DictionaryDetailComponent } from './dictionary-detail/dictionary-detail.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {DictionaryService} from "./dictionary.service";
+import { HomeComponent } from './home/home.component';
+import { TimelinesComponent } from './timelines/timelines.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    YoutubePlaylistComponent,
+    YoutubePlayerComponent,
+    DictionaryComponent,
+    DictionaryPageComponent,
+    DictionaryDetailComponent,
+    HomeComponent,
+    TimelinesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
-  providers: [],
+  providers: [DictionaryService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
